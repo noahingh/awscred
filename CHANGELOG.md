b0475b3e16e5ad9b8efec09a7694686320408845
hanjunlee <opsdownn@gmail.com>
2020-09-06 12:42:10 +0900

    Release v0.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 0.2.1
+
+### Added
+
+- Support brew.
+
 ## 0.2.0
 
 ### Added
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	version = "v0.2.0"
+	version = "v0.2.1"
 )
 
 func main() {
